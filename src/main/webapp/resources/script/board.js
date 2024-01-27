d1978d6fa30cca0c22ebb04791991d59cef4318a
javython999 <infinity2544@gmail.com>
2024-01-27 21:47:45 +0900

    댓글 수정 함수 제거

--- src/main/webapp/resources/script/board.js
+++ src/main/webapp/resources/script/board.js
@@ -11,15 +11,6 @@ function askDelete(target) {
 }
 
 
-// 댓글 수정
-function cmtModify(target, url) {
-	let targetClass = ".modify" + target;
-	let targetForm = document.querySelector(targetClass);
-	targetForm.innerHTML =
-	"<form method='post' class="+"'modify"+target+"'"+"action='/board/"+url+"/update.do'><input type='hidden' name='id' value=" + target +"><div><span>별점평가 : </span><input type='radio' id='star1' name='star' value='1'><label for='star1'><i class='fas fa-star'></i></label><input type='radio' id='star2' name='star' value='2'><label for='star2'><i class='fas fa-star'></i><i class='fas fa-star'></i></label><input type='radio' id='star3' name='star' value='3'><label for='star3'><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></label><input type='radio' id='star4' name='star' value='4'><label for='star4'><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></label><input type='radio' id='star5' name='star' value='5' checked><label for='star5'><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></label></div><div><textarea class='reply' name='content' placeholder='방문후기를 남겨주세요.'></textarea></div><input type='submit' value='수정완료' onclick='cmtModifySubmit("+target+")'></form>"	
-}
-
-
 function cmtModifySubmit(target) {
 	let targetClass = ".modify" + target;
 	let targetForm = document.querySelector(targetClass);
@@ -32,7 +23,7 @@ function cmtModifySubmit(target) {
 
 function checkValue() {
 	if (!document.querySelector(".reply").value) {
-    alert("댓글을 작성해주세요.");
-    return false;
-  }
+		alert("댓글을 작성해주세요.");
+		return false;
+	}
 }
