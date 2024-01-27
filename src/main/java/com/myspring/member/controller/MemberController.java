a0cd7f262727c9c7396a4feb7c781ac72e994a8c
javython999 <infinity2544@gmail.com>
2024-01-27 13:14:51 +0900

    @RequestMapping -> @Getmapping으로 수정

--- src/main/java/com/myspring/member/controller/MemberController.java
+++ src/main/java/com/myspring/member/controller/MemberController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
@@ -53,7 +52,7 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @RequestMapping("/member/logout")
+    @GetMapping("/member/logout")
     public String logOut(HttpSession session) {
         session.invalidate();
         return "redirect:/";
